89a2625aa2b57ddb4e7e260bf40264d9ef3aaa21
vivek singh <55220203+ui-vivek@users.noreply.github.com>
2022-12-20 12:23:59 +0530

    Update theme.js

--- theme.js
+++ theme.js
@@ -4,6 +4,18 @@ let theam=document.querySelector('data-theme')
 const html = document.querySelector('html');
 const theme = localStorage.getItem('theme');
 
+html.setAttribute('data-theme',theme);
+if(theme=='luxury'){
+    icon.className='bx bxs-sun bx-sm';
+    mode.style.backgroundColor="white";
+    html.setAttribute('data-theme', 'luxury');
+
+}else{
+    html.setAttribute('data-theme', 'bumblebee');
+    icon.className='bx bxs-moon bx-sm'
+    mode.style.backgroundColor="black";
+}
+
 function ChangeMode(){
     if(icon.className=='bx bxs-moon bx-sm'){
         icon.className='bx bxs-sun bx-sm';
